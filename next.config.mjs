4e3be58e7047b71201db1819efa9dd014678a50c
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-06-07 19:43:55 +0200

    confused with config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let assetPrefix = '';
+let basePath = '';
 
-export default nextConfig;
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+const nextConfig = {
+  basePath,
+  assetPrefix,
+  reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
+  output: 'export', // Use the new way to configure export
+};
+
+module.exports = nextConfig;
