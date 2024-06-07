d68a16642f526dfc2b5b46469d306e8a59024c21
Hamza Eljaouhari <hamza.eljaouhari.1995@gmail.com>
2024-06-07 19:28:39 +0200

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
-// src/pages/_app.tsx
-import './globals.css';
-import type { AppProps } from 'next/app';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
 }
-
-export default MyApp;
